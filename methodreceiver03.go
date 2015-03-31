67a67b5f40a629484611005118c27ad43189b1b0
Michael Angerman <stormasm@gmail.com>
2015-03-31 15:45:57 -0700

    initial commit of methodreceiver03

---
+++ methodreceiver03.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+import "math"
+
+type MyFloat float64
+
+func (x MyFloat) Abs() float64 {
+    if x < 0 {
+        return float64(-x)
+    }
+    return float64(x)
+}
+
+func main() {
+    f := MyFloat(-math.Sqrt2)
+    fmt.Println(f.Abs())
+}
