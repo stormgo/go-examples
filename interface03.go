e2d017028bc02c300bb7b678cf2c6b985bce6fbf
Michael Angerman <stormasm@gmail.com>
2016-09-20 16:35:23 -0700

    update interface03

--- interface03.go
+++ interface03.go
@@ -2,24 +2,37 @@ package main
 
 import "fmt"
 
-type Walker interface {
-	Walk(miles int)
+type Walker1 interface {
+	Walk1(miles int)
+}
+
+type Walker2 interface {
+	Walk2(miles int)
 }
 
 type Camel struct {
 	Name string
 }
 
-func (c Camel) Walk(miles int) {
+func (c Camel) Walk1(miles int) {
 	fmt.Printf("%s is walking %v miles\n", c.Name, miles)
 }
 
-func LongWalk(w Walker) {
-	w.Walk(500)
-	w.Walk(500)
+func (c *Camel) Walk2(miles int) {
+	fmt.Printf("%s is walking %v miles\n", c.Name, miles)
+}
+
+func LongWalk1(w Walker1) {
+	w.Walk1(500)
+}
+
+func LongWalk2(w Walker2) {
+	w.Walk2(600)
 }
 
 func main() {
-	c := Camel{"Bill"}
-	LongWalk(c)
+	c1 := Camel{"Bill"}
+	c2 := &Camel{"Jane"}
+	LongWalk1(c1)
+    LongWalk2(c2)
 }
