fec46905107def40471a1316419d08837446eefc
Michael Angerman <stormasm@gmail.com>
2016-09-19 19:26:49 -0700

    update pointer01

--- pointer01.go
+++ pointer01.go
@@ -13,10 +13,22 @@ func zeronp(x int) {
 func zerop(xPtr *int) {
 	*xPtr = 0
 }
+
+
+func hundred(xPtr *int) {
+	*xPtr = 100
+}
+
 func main() {
 	x := 5
 	zeronp(x)
 	fmt.Println(x) // x is 5
 	zerop(&x)
 	fmt.Println(x) // x is 0
+
+	var p *int
+	i := 42
+	p = &i
+	hundred(p)
+	fmt.Println(i) // i is 100
 }
